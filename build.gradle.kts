8f20b835a5e2676d9a77a3bcd3c4e2ca2d5cb2e4
cssxsh <cssxsh@gmail.com>
2021-08-01 08:02:24 +0800

    build: 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.gnuf0rce"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/io/gnuf0rce/mirai/plugin/DebugHelperPlugin.kt
+++ src/main/kotlin/io/gnuf0rce/mirai/plugin/DebugHelperPlugin.kt
@@ -1,20 +1,24 @@
 package io.gnuf0rce.mirai.plugin
 
 import io.gnuf0rce.mirai.plugin.data.*
+import net.mamoe.mirai.console.data.PluginConfig
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 
 object DebugHelperPlugin : KotlinPlugin(
-    JvmPluginDescription(id = "xyz.cssxsh.mirai.plugin.debug-helper", version = "1.0.0") {
+    JvmPluginDescription(id = "xyz.cssxsh.mirai.plugin.debug-helper", version = "1.0.1") {
         name("debug-helper")
         author("cssxsh")
     }
 ) {
+    private fun <T : PluginConfig> T.save() = loader.configStorage.store(this@DebugHelperPlugin, this)
 
     override fun onEnable() {
         DebugSetting.reload()
+        DebugSetting.save()
         DebugRequestEventData.reload()
         DebugOnlineConfig.reload()
+        DebugOnlineConfig.save()
         DebugCommands.registerAll()
 
         DebugSubscriber.start()
