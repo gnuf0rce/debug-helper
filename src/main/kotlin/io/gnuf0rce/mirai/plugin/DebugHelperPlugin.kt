6c14ea6ded9ebccbf22f2997e4b3bbf33a65ca4a
cssxsh <cssxsh@gmail.com>
2021-11-05 12:26:52 +0800

    build: 1.1.2

--- src/main/kotlin/io/gnuf0rce/mirai/plugin/DebugHelperPlugin.kt
+++ src/main/kotlin/io/gnuf0rce/mirai/plugin/DebugHelperPlugin.kt
@@ -8,7 +8,7 @@ import net.mamoe.mirai.console.util.*
 import net.mamoe.mirai.event.*
 
 object DebugHelperPlugin : KotlinPlugin(
-    JvmPluginDescription(id = "xyz.cssxsh.mirai.plugin.debug-helper", version = "1.1.1") {
+    JvmPluginDescription(id = "xyz.cssxsh.mirai.plugin.debug-helper", version = "1.1.2") {
         name("debug-helper")
         author("cssxsh")
     }
